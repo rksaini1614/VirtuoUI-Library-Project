9af80d5137e7858c5847a530e650f0e7af46d10b
Ritik Saini <ritikkumarsaini3.14@gmail.com>
2026-07-21 20:17:41 +0530

    Update ProtectedRoutes.jsx

--- virtuoui-client/src/components/ProtectedRoutes.jsx
+++ virtuoui-client/src/components/ProtectedRoutes.jsx
@@ -5,7 +5,11 @@ function ProtectedRoute({ children, authChecked }) {
     const { userData } = useSelector((state) => state.user);
 
     if (!authChecked) {
-        return <div>Loading...</div>;
+      return (
+        <div className="min-h-screen flex items-center justify-center">
+          <div className="h-10 w-10 border-4 border-cyan-400 border-t-transparent rounded-full animate-spin"></div>
+        </div>
+      );
     }
 
     if (!userData) {
